86fc27333ceed035850dd9555cce28cdd3b9620c
Robert Wysocki <robert.wysocki@syncron.com>
2017-07-09 02:16:43 +0200

    Format message
    Change-Id: I6ad49f9509ae8b448a349d3e0cad365cdfe1efc4

--- src/main/kotlin/com/wabadaba/codecov2discord/MainController.kt
+++ src/main/kotlin/com/wabadaba/codecov2discord/MainController.kt
@@ -1,17 +1,23 @@
 package com.wabadaba.codecov2discord
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
-import java.awt.Color
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 
 @RestController
 class MainController {
+
+    @Autowired
+    lateinit var mapper: ObjectMapper
 
     val logger = LoggerFactory.getLogger(this::class.java)
 
@@ -21,44 +27,76 @@ class MainController {
     fun test(
             @PathVariable id: String,
             @PathVariable token: String,
-            @RequestBody body: String
+            @RequestBody bodyString: String
     ) {
-        logger.info(body)
+        logger.info(bodyString)
+        val body = mapper.readValue(bodyString, CodecovWebhook::class.java)
         val template = RestTemplate(HttpComponentsClientHttpRequestFactory())
-        val mask: Int = 0xFFFFFF
-        val color = Color.CYAN.rgb and mask
-        val footer = WebhookEmbedFooter(
-                "Footer",
-                iconUrl
-        )
-        val embed = WebhookEmbed(
-                "Embed",
-                "Description",
-                iconUrl,
-                footer,
-                color)
-        val payload = WebhookPayload(
-                "Content",
+        val author = DiscordWebhook.Embed.Author(
+                body.head.author.name)
+        val commitIdShort = body.head.commitid.substring(0, 6)
+        val description =
+                "[`$commitIdShort`](${body.head.service_url}) ${body.head.message}"
+        val embed = DiscordWebhook.Embed(
+                "[${body.repo.name}:${body.head.branch}]",
+                description,
+                body.repo.url,
+                author,
+                0xF70557)
+        val webhook = DiscordWebhook(
+                "Coverage: ${body.head.totals.c.setScale(2, RoundingMode.HALF_UP)}%",
                 "Username",
                 iconUrl,
                 listOf(embed))
-        template.postForEntity("https://discordapp.com/api/webhooks/$id/$token", payload, String::class.java)
+        template.postForEntity("https://discordapp.com/api/webhooks/$id/$token", webhook, String::class.java)
     }
 }
 
-data class WebhookPayload(
+data class DiscordWebhook(
         val content: String,
         val username: String,
         val avatar_url: String,
-        val embeds: List<WebhookEmbed>)
+        val embeds: List<Embed>) {
 
-data class WebhookEmbed(
-        val title: String,
-        val description: String,
-        val url: String,
-        val footer: WebhookEmbedFooter,
-        val color: Int)
+    data class Embed(
+            val title: String,
+            val description: String,
+            val url: String,
+            val author: Author,
+            val color: Int) {
+        data class Author(
+                val name: String
+        )
+    }
+}
 
-data class WebhookEmbedFooter(
-        val text: String,
-        val icon_url: String)
+
+data class CodecovWebhook(
+        val repo: Repo,
+        val head: Head) {
+
+    data class Head(
+            val url: String,
+            val message: String,
+            val author: Author,
+            val totals: Totals,
+            val branch: String,
+            val commitid: String,
+            val service_url: String) {
+        data class Totals(
+                val c: BigDecimal
+        )
+
+        data class Author(
+                val username: String,
+                val name: String
+        )
+
+    }
+
+    data class Repo(
+            val name: String,
+            val url: String
+    )
+
+}
